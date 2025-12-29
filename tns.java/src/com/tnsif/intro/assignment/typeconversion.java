2b3acb3c27d34107fd881c6162713972d5dea050
saina <saina@LAPTOP-54T6UDK5>
2025-12-29 23:57:27 +0530

    hii

---
+++ tns.java/src/com/tnsif/intro/assignment/typeconversion.java
@@ -0,0 +1,22 @@
+package com.tnsif.intro.assignment;
+
+class typeconversion {
+	    public static void main(String[] args) {
+
+	        // Implicit Type Conversion (Widening)
+	        int a = 10;
+	        double b = a;   // int to double
+
+	        System.out.println("Implicit Type Conversion:");
+	        System.out.println("Integer value: " + a);
+	        System.out.println("Double value: " + b);
+
+	        // Explicit Type Conversion (Narrowing)
+	        double x = 25.75;
+	        int y = (int) x;   // double to int
+
+	        System.out.println("\nExplicit Type Conversion:");
+	        System.out.println("Double value: " + x);
+	        System.out.println("Integer value: " + y);
+	    }
+	}
