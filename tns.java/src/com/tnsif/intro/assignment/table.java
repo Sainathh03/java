b8a51886138308ea0df3da122f085eb19e7a7f1e
saina <saina@LAPTOP-54T6UDK5>
2025-12-30 08:44:53 +0530

    hii

---
+++ tns.java/src/com/tnsif/intro/assignment/table.java
@@ -0,0 +1,13 @@
+package com.tnsif.intro.assignment;
+
+class table {
+        public static void main(String[] args) {
+        	int num=8;
+        	System.out.println("Multiplication table of" + num);
+        	for (int i =1; 1<=10; i++)
+        	{
+        		System.out.println(num + "x" + i + "=" + (num*i));
+        		
+        	}
+        }
+}
